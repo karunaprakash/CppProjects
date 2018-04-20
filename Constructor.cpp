e4b2a58f48c349c45d9192fbdec97d5fae464546
karunaprakash <38559237+karunaprakash@users.noreply.github.com>
2018-04-20 11:46:55 +0000

    Constructor
    na

---
+++ Constructor.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class abc
+{
+  public:
+  abc(){      cout<<"Constructor"<<endl;  }
+  ~abc(){      cout<<"Destructor"<<endl;  }
+  abc(const abc& temp){cout<<"Copy Constructor"<<endl;}
+  abc& operator=(abc& temp){
+      if ( &temp != this){
+      cout<<"Objects are not same"<<endl;
+      }
+      return *this;
+      
+  }
+};
+int main() {
+abc obj1;
+abc obj2 = obj1;
+abc obj3(obj2);
+
+obj3 = obj2;
+obj3 = obj3;
+    return 0;
+}
